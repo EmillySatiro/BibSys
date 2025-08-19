15c562f511bcceada53c7686b50865cbc40add0d
Alef Cauan Sousa Rodriguês <alef.rodrigues@ufpi.edu.br>
2025-08-19 11:22:12 -0300

    feat: server init

---
+++ server/src/server.ts
@@ -0,0 +1,53 @@
+import fastify = require("fastify");
+
+const sp = require('./supabaseConnection');
+
+const app = fastify();
+
+type Users = {
+    name: string,
+    email: string
+}
+
+app.get("/users", async () => {
+    try {
+        const {data: users} = await sp.from("users").select("*")
+        return {value: users}
+    } catch(error) {
+        console.error(error)
+        throw error
+    }
+})
+
+app.post("/users", async (request, response) => {
+    try {
+        const {name, email} = request.body as Users
+
+        const {data: createdUser} = await sp.from("users").insert([
+            name,
+            email
+        ])
+
+        return {
+            value: createdUser ? createdUser[0] : null
+        }
+
+    } catch(error) {
+        console.error(error)
+        throw error
+    }
+})
+
+app.listen(
+  {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  },
+  (err) => {
+    if (err) {
+      console.error('Error starting server:', err);
+      process.exit(1);
+    }
+    console.log('Server working');
+  }
+);
---
+++ server/src/supabaseConnection.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+const { createClient } = require('@supabase/supabase-js');
+import dotenv = require('dotenv');
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseKey = process.env.SUPABASE_KEY || '';
+
+const supabase = createClient(supabaseUrl, supabaseKey, {
+    auth: {
+        persistSession: false,
+        // autoRefreshToken: true,
+    },
+});
+
+module.exports = supabase
